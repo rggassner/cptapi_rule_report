99ac8ba1dad09a40586af9087f8ded8176e42eda
rggassner <rafael.gassner@gmail.com>
2025-09-04 00:37:57 -0300

    Create translations.py

---
+++ translations.py
@@ -0,0 +1,34 @@
+translations = {
+    "en": {
+        "rule_enabled": "Enabled",
+        "rule_disabled": "Disabled",
+        "negated": "Negated ",
+        "unknown_error": "Unknown error.",
+        "rulebase_not_found": "Rulebase not found in query domain {} layer {}",
+        "empty_section": "Empty section name",
+        "report_title": "Firewall Rules Report - Restricted Access to Requester",
+        "disclaimer": "Disclaimer",
+        "specific_dst": "Specific to destination address {}:",
+        "specific_src": "Specific to source address {}:",
+        "group_dst": "Generic for groups containing destination address {}:",
+        "group_src": "Generic for groups containing source address {}:",
+        "generic": "Generic",
+        "table_header": "<table border=1>\n<tr><th>Domain</th><th>Policy</th><th>Section</th><th>Number</th><th>Status</th><th>Name</th><th>Source</th><th>Destination</th><th>Service</th><th>Action</th><th>Comment</th></tr>\n"
+    },
+    "pt": {
+        "rule_enabled": "Habilitada",
+        "rule_disabled": "Desabilitada",
+        "negated": "Negado ",
+        "unknown_error": "Erro desconhecido.",
+        "rulebase_not_found": "Rulebase não encontrada no domínio {} camada {}",
+        "empty_section": "Nome de seção vazio",
+        "report_title": "Relatório de Regras de Firewall - Acesso Restrito ao Solicitante",
+        "disclaimer": "Prezado,",
+        "specific_dst": "Específicas para o endereço de destino {}:",
+        "specific_src": "Específicas para o endereço de origem {}:",
+        "group_dst": "Genéricas para grupos contendo o endereço de destino {}:",
+        "group_src": "Genéricas para grupos contendo o endereço de origem {}:",
+        "generic": "Genéricas",
+        "table_header": "<table border=1>\n<tr><th>Domínio</th><th>Política</th><th>Seção</th><th>Número</th><th>Status</th><th>Nome</th><th>Origem</th><th>Destino</th><th>Serviço</th><th>Ação</th><th>Comentário</th></tr>\n"
+    }
+]
